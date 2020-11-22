ef243aec7e4d33757fb7ac005fca641e6f9552a9
xX-Conan-Xx <61041807+xX-Conan-Xx@users.noreply.github.com>
2020-11-22 18:14:38 +0800

    Update 3

--- src/LeaveEvent.java
+++ src/LeaveEvent.java
@@ -2,16 +2,11 @@ import java.util.List;
 
 public class LeaveEvent extends Event{
     public LeaveEvent(Customer customer, List<Server> servers) {
-        super(customer.getArrivalTime(), customer, servers);
+        super(customer.getArrivalTime(), customer, x->Pair.of(x,null));
     }
 
     @Override
     public String toString() {
         return String.format("%.3f", super.getTime()) + " " + getCustomer().getCustomerID() + " leaves";
-    }
-
-    @Override
-    public Event execute() {
-        return null;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,9 @@ public class Main {
                 //shops.replace(s)
                 //shops.replace(s).find(x -> x.isAvailable())
                 //shops
-                pair.first()
+                //pair.first()
+                new ArriveEvent(new Customer(1, 1.0)).execute(new Shop(List.of(new Server(1,false,false,1.0)))).second()
+
         );
     }
 }
