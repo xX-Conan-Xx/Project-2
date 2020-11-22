d19ab4a68a0697dfafed53aa4b327524c4864e44
xX-Conan-Xx <61041807+xX-Conan-Xx@users.noreply.github.com>
2020-11-22 17:24:54 +0800

    Update 2

--- src/ServeEvent.java
+++ src/ServeEvent.java
@@ -1,11 +1,18 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ServeEvent extends Event{
     private final Server newServer;
     public ServeEvent( Customer customer, List<Server> servers, Server server) {
-        super(Math.max(server.getNextAvailableTime(),customer.getArrivalTime()), customer, servers);
+        super(Math.max(server.getNextAvailableTime(),customer.getArrivalTime()), customer, x->{
+            double thisTime = Math.max(server.getNextAvailableTime(),customer.getArrivalTime());
+            List<Server> editedServers = new ArrayList<>();
+            editedServers.addAll(x.getServers());
+            editedServers.set(servers.indexOf(server),new Server(server.getIdentifier(),false,false, thisTime+1));
+            return Pair.of(new Shop(editedServers),
+                    new DoneEvent(customer,x.getServers(), new Server(server.getIdentifier(),false,false, thisTime+1)));
+        });
         this.newServer = new Server(server.getIdentifier(),false,false, this.getTime()+1);
-        this.getServers().set(servers.indexOf(server),newServer);
     }
 
     @Override
@@ -13,8 +20,4 @@ public class ServeEvent extends Event{
         return String.format("%.3f", this.getTime()) + " " + getCustomer().getCustomerID() + " served by " + newServer.getIdentifier();
     }
 
-    @Override
-    public Event execute() {
-        return new DoneEvent(this.getCustomer(),this.getServers(),this.newServer);
-    }
 }
